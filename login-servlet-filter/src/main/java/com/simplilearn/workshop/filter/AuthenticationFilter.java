e8c670f84ab19824f3e9a9c671b46c9cb2c51f96
Rinaldo Ramos <rinaldoramos87@gmail.com>
2021-05-22 10:51:17 -0400

    Adding project login-servlet-filter. Initial commit.

---
+++ login-servlet-filter/src/main/java/com/simplilearn/workshop/filter/AuthenticationFilter.java
@@ -0,0 +1,51 @@
+package com.simplilearn.workshop.filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/secured-servlet")
+public class AuthenticationFilter implements Filter {
+
+	public AuthenticationFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void destroy() {
+		System.out.println("destroy method is called in " + this.getClass().getName());
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("doFilter method is called in " + this.getClass().getName());
+
+		//read the parameters
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		//obtatin the client IP address
+		String ipAddress = request.getRemoteAddr();
+		
+		if (username.equals("simplilearn") && password.equals("simplilearn")) {
+			System.out.println("User logged in " + ipAddress + " at " + new Date().toString());
+			// pass the request along the filter chain
+			chain.doFilter(request, response);
+		}else {
+			PrintWriter out = response.getWriter();
+			out.println("<h3>Sorry, you are not authorizrd to access this resource</h3>");
+		}	
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		System.out.println("init method is called in " + this.getClass().getName());
+	}
+
+}
